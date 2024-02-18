8e04d7b93f6ed36405151e1953908441cb507d85
yogesh1663 <yogesh.code23@gmail.com>
2024-02-18 11:46:48 +0545

    resource route was made

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,7 @@
+<x-user.front-layout>
+    <div class="card">
+        <div class="card-body">
+            This is some text within a card body.
+        </div>
+    </div>
+</x-user.front-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::class, 'index'])->name('home');
+// Route::get('/', [UserController::class, 'index'])->name('home');
+Route::resource('users', UserController::class);
